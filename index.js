257bb123914f647b543657755026a9d830a7fcca
Replit user <>
2023-12-24 13:20:45 +0000

    micro service url short

--- index.js
+++ index.js
@@ -1,24 +1,52 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const dns = require("dns");
+const urlParser = require("url");
+const { MongoClient } = require("mongodb");
 
+const client = new MongoClient(process.env.MONGO_URL);
+const db = client.db("urlshort");
+const urls = db.collection("short");
+
+app.use(express.urlencoded({ extended: true }));
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post("/api/shorturl", function (req, res) {
+  const url = req.body.url;
+  const urlObj = urlParser.parse(url);
+  const dnslookup = dns.lookup(urlObj.hostname, async (err, address) => {
+    if (!address) {
+      res.json({ error: "invalid url" });
+    } else {
+      const urlCount = await urls.countDocuments({});
+      const urlDoc = {
+        url,
+        short_url: urlCount,
+      };
+      const final = await urls.insertOne(urlDoc);
+      res.json({ original_url: url, short_url: urlCount });
+    }
+  });
 });
 
-app.listen(port, function() {
+app.get("/api/shorturl/:short_url", async (req, res) => {
+  const short_url = req.params.short_url;
+  const urlDoc = await urls.findOne({ short_url: +short_url });
+  res.redirect(urlDoc.url);
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
